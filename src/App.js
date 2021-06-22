058c110609e4dade41fc735816762add3f181707
Ari Swedberg <akswedberg@gmail.com>
2021-06-23 00:31:28 +0200

    add initial landing page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header></header>
+      <section className="landing-body">
         <h1>Duckflix</h1>
+        <br></br>
         <img src='assets/images/temp-duckflix-logo.png' className="App-logo" alt="logo" />
         <br></br>
         <p>
-          Coming soon to a home theatre near you...
+        Coming soon to a home theatre near you...
         </p>
-      </header>
+      </section>
+      <footer>Find us on Github!</footer>
     </div>
   );
 }
